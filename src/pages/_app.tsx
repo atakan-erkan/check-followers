ee945e1b0104a51275db36510fa60e35da3f1e6e
Atakan Erkan <dev.atakan.erkan@gmail.com>
2025-05-01 00:56:00 +0300

    Added @vercel/speed-insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from "next/app";
 import { AppProvider } from '../context/AppContext';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppProvider>
+      <SpeedInsights />
       <Navbar />
       <Component {...pageProps} />
       <Footer />
